ff0419bb879f29744e08b03157e403ea3b4cb188
giovannihernandezm26@gmail.com <giovannihernandezm26@gmail.com>
2020-07-11 23:55:25 -0500

    Added dark mode button

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -8,6 +8,11 @@ const Header = ()=>{
           </div>
           <div class="Header-nav">
             <a href="#/about/"> About</a>
+            <div class="switch-box" id="switch-box">
+        <i class="fas fa-sun"></i>
+        <div class="switch" id="switch-btn"></div>
+        <i class="fas fa-moon"></i>
+    </div>
           </div>
         </div>
   `;
