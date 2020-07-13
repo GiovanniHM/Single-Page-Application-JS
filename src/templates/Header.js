e7f8f6036781bd6e5ed38caca8c0dabf3143b8f8
giovannihernandezm26@gmail.com <giovannihernandezm26@gmail.com>
2020-07-13 18:12:40 -0500

    Added logo in header section

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -2,10 +2,10 @@ const Header = ()=>{
   const view = `
   <div class="Header-main">
           <div class="Header-logo">
-            <h1>
-              <a href="/Single-Page-Application-JS">100tifi.com</a>
-            </h1>
-            
+              <a href="/Single-Page-Application-JS">
+              <img alt="Logo" src='../src/img/logo.png'
+               width=150" height="70">
+              </a>          
           </div>
             <div class="Header-nav">
             <a href="#/about/"> About</a>
@@ -23,6 +23,14 @@ const Header = ()=>{
   function terminos_cambio(checkbox){
 
     document.body.classList.toggle('dark');
+// Guardamos el modo en localstorage.
+    if(document.body.classList.contains('dark')){
+      localStorage.setItem('dark-mode', 'true');
+    } else {
+      localStorage.setItem('dark-mode', 'false');
+    }
+
+
   }
   ;
   return view;
